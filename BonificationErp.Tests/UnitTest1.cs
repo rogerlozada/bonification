357fc30a0f04975cb8273e2f209ad316c3e5e638
roger <roger.cano@dotz.com>
2020-07-09 11:01:46 -0300

    fix

--- BonificationErp.Tests/UnitTest1.cs
+++ BonificationErp.Tests/UnitTest1.cs
@@ -9,7 +9,13 @@ namespace BonificationErp.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(Operacoes.somar(2,2) == 5);
+            Assert.IsTrue(Operacoes.somar(2,2) == 4);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(Operacoes.substrair(2,2) == 0);
         }
     }
 }
